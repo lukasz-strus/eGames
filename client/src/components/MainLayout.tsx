80e851637b6b4c1b9040204447ec8c1bd7c6344b
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2024-11-29 22:04:54 +0100

    Adding login and logout

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -1,29 +1,68 @@
-import React from 'react'
-import { Container, Nav, Navbar, Button, NavDropdown, Image } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { Container, Nav, Navbar, Button, NavDropdown, Image, Modal, Alert } from 'react-bootstrap'
 import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Form } from 'react-bootstrap'
 import brandImg from '../assets/egames-logo.png'
+import { getEmail, loginUser } from '../services/api'
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
 	const [userName, setUserName] = useState<string | null>('Guest')
+	const [showLoginModal, setShowLoginModal] = useState(false)
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [loginError, setLoginError] = useState<string | null>(null)
 	const location = useLocation()
+
+	useEffect(() => {
+		const profile = async (token: string) => {
+			const email = await getEmail(token)
+
+			return email
+		}
+
+		const token = localStorage.getItem('authToken')
+		if (token) {
+			profile(token)
+				.then(reult => {
+					setIsLoggedIn(true)
+					setUserName(reult)
+				})
+				.catch(error => {
+					console.error('Failed to get user profile:', error)
+					localStorage.removeItem('authToken')
+				})
+		}
+	}, [])
 
 	const getNavLinkClass = (path: string): string => {
 		return location.pathname === path ? 'fw-bold' : ''
 	}
 
-	const handleLogin = () => {
-		// Logika logowania
-		setIsLoggedIn(true)
-		setUserName('JohnDoe') // Tymczasowa nazwa użytkownika
+	const handleLogin = async (email: string, password: string) => {
+		try {
+			const token = await loginUser(email, password)
+			localStorage.setItem('authToken', token)
+
+			setIsLoggedIn(true)
+			setUserName(email)
+			setLoginError(null) // Wyczyszczenie błędu po udanym logowaniu
+			handleCloseLoginModal()
+		} catch (error) {
+			console.error('Login failed:', error)
+			setLoginError('Invalid email or password. Please try again.') // Ustawienie błędu
+		}
 	}
 
 	const handleLogout = () => {
-		// Logika wylogowania
+		localStorage.removeItem('authToken')
 		setIsLoggedIn(false)
 		setUserName(null)
 	}
+
+	const handleShowLoginModal = () => setShowLoginModal(true)
+	const handleCloseLoginModal = () => setShowLoginModal(false)
 
 	return (
 		<>
@@ -49,7 +88,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 									<NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
 								</NavDropdown>
 							) : (
-								<Button variant='outline-light' onClick={handleLogin}>
+								<Button variant='outline-light' onClick={handleShowLoginModal}>
 									Login
 								</Button>
 							)}
@@ -57,6 +96,59 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
+			<Modal show={showLoginModal} onHide={handleCloseLoginModal}>
+				<Modal.Header closeButton>
+					<Modal.Title>Login</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{loginError && (
+						<Alert variant='danger' className='mb-3'>
+							{loginError}
+						</Alert>
+					)}
+					<Form>
+						<Form.Group className='mb-3' controlId='formUsername'>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								type='email'
+								placeholder='Enter email'
+								value={username}
+								isInvalid={!!loginError} // Dodanie walidacji Bootstrap
+								onChange={e => {
+									setUsername(e.target.value)
+									setLoginError(null) // Czyszczenie błędu przy zmianie danych
+								}}
+							/>
+						</Form.Group>
+						<Form.Group className='mb-3' controlId='formPassword'>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Enter password'
+								value={password}
+								isInvalid={!!loginError} // Dodanie walidacji Bootstrap
+								onChange={e => {
+									setPassword(e.target.value)
+									setLoginError(null) // Czyszczenie błędu przy zmianie danych
+								}}
+							/>
+							<Form.Control.Feedback type='invalid'>Invalid email or password.</Form.Control.Feedback>
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={handleCloseLoginModal}>
+						Close
+					</Button>
+					<Button
+						variant='primary'
+						onClick={async () => {
+							await handleLogin(username, password)
+						}}>
+						Login
+					</Button>
+				</Modal.Footer>
+			</Modal>
 			<Container>{children}</Container>
 		</>
 	)
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -5,20 +5,41 @@ const API = axios.create({
 	baseURL: 'https://localhost:7164/api',
 })
 
+export const loginUser = async (email: string, password: string): Promise<string> => {
+	const { data } = await API.post('/identity/login', { email, password })
+
+	return data.accessToken
+}
+
+export const getEmail = async (token: string): Promise<string> => {
+	const { data } = await API.get('/identity/manage/info', {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	})
+
+	return data.email
+}
+
 export const fetchGames = async (): Promise<Game[]> => {
 	const { data } = await API.get('/games')
+
 	return data.items
 }
 
 export const fetchGameById = async (gameId: string, gameType: string): Promise<Game> => {
 	let gameTypeUrl: string = ''
 
-	if (gameType === 'FullGame') gameTypeUrl = 'full/'
-	else if (gameType === 'DlcGame') gameTypeUrl = 'dlc/'
-	else if (gameType === 'Subscription') gameTypeUrl = 'subscriptions/'
+	gameTypeUrl = getGameType(gameType, gameTypeUrl)
 
 	const { data } = await API.get(`/games/${gameTypeUrl}${gameId}`)
 
+	return mapGameDataByType(gameType, data)
+}
+
+export default API
+
+function mapGameDataByType(gameType: string, data: any) {
 	switch (gameType) {
 		case 'FullGame':
 			return data as FullGame
@@ -31,4 +52,19 @@ export const fetchGameById = async (gameId: string, gameType: string): Promise<G
 	}
 }
 
-export default API
+function getGameType(gameType: string, gameTypeUrl: string) {
+	switch (gameType) {
+		case 'FullGame':
+			gameTypeUrl = 'full/'
+			break
+		case 'DlcGame':
+			gameTypeUrl = 'dlc/'
+			break
+		case 'Subscription':
+			gameTypeUrl = 'subscriptions/'
+			break
+		default:
+			throw new Error(`Unknown game type: ${gameType}`)
+	}
+	return gameTypeUrl
+}
